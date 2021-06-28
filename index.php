297969c70156e5d71938231530b0268384510c26
SophieeV <sophievesin@wanadoo.fr>
2021-06-28 14:33:33 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExerciceI5</title>
+</head>
+<body>
+<?php
+echo "le timestamp courant est ". time();
+  echo "<br>";
+ 
+  $time1 = mktime(2, 1, 2021);
+  echo 'Timestamp 01 Février 2021 : ' .$time1. '<br>';
+  ?>
+</body>
+</html>
